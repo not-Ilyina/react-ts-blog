f7af8282c7d722ec1d7b2cce6d84d870ce46a47b
Ilyina <Weightsword@outlook.com>
2024-06-09 17:35:54 +0800

    add axios

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,14 @@
 "use client";
+import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function About() {
-
+  useEffect(() => {
+    axios.get('http://localhost:5000/').then((res) => {
+      console.log(res.data);
+    })
+  }, []);
   const router = useRouter();
   return (
     <main>
