c90b0887f0ff2195ff2b3c1670c60e0d4d6483b6
Ilyina <Weightsword@outlook.com>
2024-06-13 20:35:11 +0800

    article page base

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,22 +1,39 @@
 "use client";
 
+import axios from "axios";
 import { usePathname, useSearchParams } from "next/navigation";
 // import axios from "axios";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { formatDate } from "../utils/format_date";
 export default function Article() {
-
-    const searchParams = useSearchParams();
+    const [articleInfo, setArticleInfo] = useState<ArticleInfoItem>({} as ArticleInfoItem);
+    const searchParams = useSearchParams()
+    const id = searchParams.get('id');
     useEffect(() => {
-        // axios.get('api/user/list').then((res) => {
-        //   console.log(res.data);
-        // })
-        console.log(searchParams.get('id'));
+        axios.get('api/article', {
+            params: {
+                id,
+            },
+        }).then((res) => {
+            setArticleInfo(res.data.data);
+        });
+        // console.log(searchParams.get('id'));
     }, []);
 
     return (
-    <main>
-        <h1>Article</h1>
-    </main>
+        <div>
+            <h1>{articleInfo.title}</h1>
+            <div>
+                <span>{formatDate(articleInfo.mtime)}</span>
+                <span>评论 {0}</span>
+                <span>阅读 { articleInfo.read }</span>
+                <span>点赞 { articleInfo.like }</span>
+                <span>收藏 { articleInfo.collection }</span>
+            </div>
+            <div>
+                文章内容。。。。。。
+            </div>
+        </div>
     );
 }
